91a42bc15ea874f6c0b13435ee4ef2f752cf17bc
Kaleab Tibebu <kaleabtibebu400@gmail.com>
2023-12-20 14:46:24 +0300

    Added solution for Reverse String

--- python_track/reverse-string.py
+++ python_track/reverse-string.py
@@ -3,11 +3,8 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s)-1
-        while left <= right:
-            s[left], s[right] = s[right], s[left]
-            left+=1
-            right-=1
-        
-        
+        def newfunc(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                newfunc(left + 1, right - 1)
+        newfunc(0, len(s) - 1)
