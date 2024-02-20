ebf95c18cbfa83ca3d425c786bd43e2e975d56f3
Kaleab Tibebu <kaleabtibebu400@gmail.com>
2024-02-20 18:46:55 +0300

    Add solution for Count Good Numbers

---
+++ camp1/leetcode/count-good-numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:      
+        def countGood(power: int, x: int) -> int:
+            if power == 0:
+                return 1  
+            elif power % 2 == 0:
+                return countGood(power // 2, x * x % MOD)
+            return x * countGood(power - 1, x) % MOD
+
+        MOD = 10 ** 9 + 7
+        return 5 ** (n % 2) * countGood(n // 2, 4 * 5) % MOD
