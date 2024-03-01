a43e13c822c562c9022b5460b7c92f9e7ce16fc6
Kaleab Tibebu <kaleabtibebu400@gmail.com>
2024-03-01 11:14:39 +0300

    Add solution for Range Sum Query 2D - Immutable

--- camp1/leetcode/range-sum-query-2d-immutable.py
+++ camp1/leetcode/range-sum-query-2d-immutable.py
@@ -1,12 +1,42 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.dp=[[0] * (len(matrix[0])+1) for _ in range(len(matrix)+1)]
- 
-        for r in range(len(self.dp)-1):
-            for c in range(len(self.dp[0])-1):
-                self.dp[r+1][c+1]=matrix[r][c] + self.dp[r][c+1] + self.dp[r+1][c] - self.dp[r][c]
-        
+        self.matrix = matrix
+        n, m = len(matrix), len(matrix[0])
+
+        self.ps = []
+        for _ in range(n):
+            self.ps.append([0]*m)
+
+        for r in range(n):
+            for c in range(m):
+                self.psFinder(r, c)
+    
+    def psFinder(self, i, j):
+        if i-1<0:
+            self.ps[i][j] = self.ps[i][j-1]+self.matrix[i][j]
+        elif j-1<0:
+            self.ps[i][j] = self.ps[i-1][j]+self.matrix[i][j]
+        else:
+            self.ps[i][j] = self.ps[i-1][j]+self.ps[i][j-1]-self.ps[i-1][j-1]+self.matrix[i][j]
+
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.dp[row2+1][col2+1] - self.dp[row1][col2+1] - self.dp[row2+1][col1] + self.dp[row1][col1]
-                
+        curr = self.ps[row2][col2]
+
+        suml = 0
+        if col1-1>=0:
+            suml = self.ps[row2][col1-1]
+
+        sumt = 0
+        if row1-1>=0:
+            sumt = self.ps[row1-1][col2]
+
+        sumd = 0
+        if row1-1>=0 and col1-1>=0:
+            sumd = self.ps[row1-1][col1-1]
+
+        return curr-suml-sumt+sumd
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
