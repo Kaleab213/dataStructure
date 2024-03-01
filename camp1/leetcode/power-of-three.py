b4061f95b1f9bde10611e03a8133288916bcbab3
Kaleab Tibebu <kaleabtibebu400@gmail.com>
2024-03-01 16:32:55 +0300

    Add solution for Power of Three

---
+++ camp1/leetcode/power-of-three.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        while n%3 == 0 and n!=0:
+            n = n/3
+        if n == 1:
+            return True
+        return False
